4f4567ef80344ba24be0e82d573f3f06524bbecc
takoyakki0609 <kjy6478@naver.com>
2024-02-19 15:00:11 +0900

    edit url

--- src/components/workslide/modal.styled.js
+++ src/components/workslide/modal.styled.js
@@ -2,9 +2,7 @@ import styled from '@emotion/styled';
 
 export const Container = styled('section')({
   position: 'absolute',
-  top: '0',
-  left: '25%',
-  right: '0',
+  top: '-10%',
   bottom: '0',
   backgroundColor: 'rgba(0, 0, 0, 0.5)',
   display: 'flex',
@@ -13,7 +11,7 @@ export const Container = styled('section')({
   zIndex: '1000',
 
   width: '850px',
-  height: '700px',
+  height: '600px',
   boxShadow:
     'rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px',
 });
